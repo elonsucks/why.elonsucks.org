4bcc5931449ce7ea544127faf80ce4a13da1d4c6
Adam Lui 刘展鹏 <adam@elonsucks.org>
2023-11-03 06:38:53 -0700

    Removed outer obj, replaced tabs w/ spaces

--- js/urls.js
+++ js/urls.js
@@ -1,62 +1,60 @@
-var redirectURLs = {
-	"urls" : [
-	// Articles
-		{ "url" : "https://www.vanityfair.com/news/2022/04/elon-musk-twitter-terrible-things-hes-said-and-done" },
-		{ "url" : "https://www.businessinsider.com/spacex-paid-250000-to-a-flight-attendant-who-accused-elon-musk-of-sexual-misconduct-2022-5" },
-		{ "url" : "https://www.indepthnews.net/index.php/opinion/3735-we-will-coup-whoever-we-want-elon-musk-and-the-overthrow-of-democracy-in-bolivia" },
-		{ "url" : "https://www.independent.co.uk/tech/elon-musk-neuralink-brain-chip-monkey-b2014551.html" },
-		{ "url" : "https://www.protocol.com/tesla-nhtsa-recall" },
-		{ "url" : "https://www.nbcnews.com/tech/tech-news/disgruntled-neighbors-dwindling-shorebirds-jeopardize-spacex-expansion-rcna7792" },
-		{ "url" : "https://www.bloomberg.com/news/articles/2021-12-15/chinese-customers-in-court-over-tesla-legal-strategy-to-silence-critics" },
-		{ "url" : "https://www.theguardian.com/technology/2017/jun/01/tesla-fires-aj-vandermeyden-lawsuit-sexual-harrassment" },
-		{ "url" : "https://www.newsnationnow.com/business/6-more-women-allege-sexual-harassment-at-tesla/" },
-		{ "url" : "https://www.cnbc.com/2021/12/14/former-spacex-engineer-essay-alleges-culture-is-rife-with-sexism.html" },
-		{ "url" : "https://www.inputmag.com/culture/five-ex-spacex-employees-allege-widespread-sexual-misconduct" },
-		{ "url" : "https://jezebel.com/elon-musk-is-a-huge-free-speech-advocate-but-if-you-cr-1849077015" },
-		{ "url" : "https://www.inputmag.com/culture/spacex-employees-open-letter-bash-elon-musk" },
-		{ "url" : "https://www.theguardian.com/technology/2022/feb/18/tesla-california-racial-harassment-discrimination-lawsuit" },
-		{ "url" : "https://jezebel.com/elon-musks-trans-daughter-files-to-change-her-name-no-1849086311" },
-		{ "url" : "https://www.cnn.com/2022/06/17/tech/spacex-letter-musk-firing/index.html" },
-		{ "url" : "https://www.theverge.com/2022/6/17/23172913/spacex-complaint-letter-firing-elon-musk" },
-		{ "url" : "https://www.motorbiscuit.com/elon-musk-wanted-tesla-co-founder-badly-took-lawsuit-make-legal-paper/" },
-		{ "url" : "https://www.dailypress.com/business/ct-biz-tesla-braking-investigation-ap-20220603-lc5lygeia5hy3jbggzo32uewxq-story.html" },
-		{ "url" : "https://www.theverge.com/2022/6/17/23172915/elon-musk-spacex-letter-fired-legal-protected-speech-nlrb" },
-		{ "url" : "https://www.standard.co.uk/business/tesla-discrimination-elon-musk-racism-electric-cars-b1010093.html" },
-		{ "url" : "https://www.cnn.com/2022/08/10/business/nightcap-elon-musk-spacex-cpi-dominos/index.html" },
-		{ "url" : "https://www.seattletimes.com/business/elon-musks-antics-turn-owners-and-would-be-buyers-against-tesla/" },
-		{ "url" : "https://news.yahoo.com/elon-musk-scolds-tesla-driver-153215907.html" },
-		{ "url" : "https://www.cnbc.com/2022/08/05/california-dmv-says-tesla-fsd-autopilot-marketing-deceptive.html" },
-		{ "url" : "https://www.usatoday.com/story/money/2022/09/15/tesla-elon-musk-sued-autopilot/10386771002/" },
-		{ "url" : "https://arstechnica.com/tech-policy/2022/11/spacex-faces-labor-charges-after-firing-employees-who-criticized-elon-musk/" },
-		{ "url" : "https://www.theverge.com/2022/11/20/23469616/tesla-recalls-over-300000-vehicles-taillight-issue-airbags" },
-		{ "url" : "https://www.yahoo.com/news/tesla-issues-back-back-recalls-061456829.html" },
-		{ "url" : "https://www.reuters.com/legal/exclusive-tesla-faces-us-criminal-probe-over-self-driving-claims-sources-2022-10-26/" },
-		{ "url" : "https://www.popsci.com/technology/twitter-covid-misinformation/" },
-		{ "url" : "https://www.theverge.com/23551060/elon-musk-twitter-takeover-layoffs-workplace-salute-emoji" },
-		{ "url" : "https://www.msn.com/en-us/autos/news/emails-show-elon-musk-was-directly-involved-in-staged-self-driving-video/ar-AA16z1XM" },
-		{ "url" : "https://www.nbcbayarea.com/news/local/east-bay/tesla-through-wall-san-ramon/3150489/" },
-		{ "url" : "https://fortune.com/2023/02/09/steve-wozniak-says-dishonest-elon-musk-cheated-tesla-buyers/" },
-		{ "url" : "https://www.platformer.news/p/elon-musk-fires-a-top-twitter-engineer" },
-		{ "url" : "https://www.theguardian.com/technology/2023/feb/15/elon-musk-changes-twitter-algorithm-super-bowl-slump-report" },
-		{ "url" : "https://www.forbes.com/sites/mattnovak/2023/02/20/yes-twitter-gives-a-warning-to-users-who-liked-a-tweet-criticizing-tesla-but-theres-more-to-the-story" },
-		{ "url" : "https://www.nbcbayarea.com/news/local/east-bay/tesla-crash-automated-driving-system/3174530/" },
-		{ "url" : "https://www.scmp.com/tech/big-tech/article/3231488/tesla-did-not-fix-autopilot-after-fatal-crash-engineers-say" },
-		{ "url" : "https://www.theguardian.com/technology/2023/sep/08/elon-musk-committed-evil-starlink-order-ukraine" },
-	// Videos		
-		{ "url" : "https://www.youtube.com/watch?v=IAsWX8QKROo" }, // Elon Musk’s Neuralink Trials Go HORRIBLY Wrong - TYT
-		{ "url" : "https://youtu.be/UsuYxFBHsiQ" }, // TOP TESLA FIRES COMPILATION VIDEO! Watch as these Tesla cars & batteries catch on fire and explode! - Random Earth
-		{ "url" : "https://www.youtube.com/watch?v=7xzd_xlkPgw" }, //  Gravitas: 1500 animals killed in Neuralink trials - WION
-		{ "url" : "https://www.msnbc.com/mehdi-on-msnbc/watch/what-elon-musk-s-china-ties-mean-for-tesla-twitter-and-how-it-could-endanger-the-rest-of-us-163939397764" },
-		{ "url" : "https://www.youtube.com/watch?v=-Xv9AdwtfD8" }, // Fatal Tesla out-of-control crash - SCMP
-	// Quora
-		{ "url" : "https://www.quora.com/Whats-the-worst-thing-about-Elon-Musk" },
-	// Tesla lawsuits
-		{ "url" : "https://cdn.elonsucks.org/tesla/lynch-et-al-v-tesla-warn-act-class-action.pdf" },
-		{ "url" : "https://cdn.elonsucks.org/tesla/isaacs-v-musk-tesla-securities-manipulation-class-action.pdf" },
-		{ "url" : "https://cdn.elonsucks.org/tesla/williams-et-al-v-tesla-defective-suspension-class-action.pdf" },
-		{ "url" : "https://cdn.elonsucks.org/tesla/california-v-tesla-racial-discrimination-complaint.pdf" },
-	// Twitter lawsuits
-		{ "url" : "https://cdn.elonsucks.org/twitter/cornet-et-al-v-twitter-warn-act-class-action.pdf" },
-		{ "url" : "https://cdn.elonsucks.org/twitter/strifling-et-al-v-twitter-female-discrimination-class-action.pdf" }
-	]
-}
+const urls = [
+// Articles
+    { "url" : "https://www.vanityfair.com/news/2022/04/elon-musk-twitter-terrible-things-hes-said-and-done" },
+    { "url" : "https://www.businessinsider.com/spacex-paid-250000-to-a-flight-attendant-who-accused-elon-musk-of-sexual-misconduct-2022-5" },
+    { "url" : "https://www.indepthnews.net/index.php/opinion/3735-we-will-coup-whoever-we-want-elon-musk-and-the-overthrow-of-democracy-in-bolivia" },
+    { "url" : "https://www.independent.co.uk/tech/elon-musk-neuralink-brain-chip-monkey-b2014551.html" },
+    { "url" : "https://www.protocol.com/tesla-nhtsa-recall" },
+    { "url" : "https://www.nbcnews.com/tech/tech-news/disgruntled-neighbors-dwindling-shorebirds-jeopardize-spacex-expansion-rcna7792" },
+    { "url" : "https://www.bloomberg.com/news/articles/2021-12-15/chinese-customers-in-court-over-tesla-legal-strategy-to-silence-critics" },
+    { "url" : "https://www.theguardian.com/technology/2017/jun/01/tesla-fires-aj-vandermeyden-lawsuit-sexual-harrassment" },
+    { "url" : "https://www.newsnationnow.com/business/6-more-women-allege-sexual-harassment-at-tesla/" },
+    { "url" : "https://www.cnbc.com/2021/12/14/former-spacex-engineer-essay-alleges-culture-is-rife-with-sexism.html" },
+    { "url" : "https://www.inputmag.com/culture/five-ex-spacex-employees-allege-widespread-sexual-misconduct" },
+    { "url" : "https://jezebel.com/elon-musk-is-a-huge-free-speech-advocate-but-if-you-cr-1849077015" },
+    { "url" : "https://www.inputmag.com/culture/spacex-employees-open-letter-bash-elon-musk" },
+    { "url" : "https://www.theguardian.com/technology/2022/feb/18/tesla-california-racial-harassment-discrimination-lawsuit" },
+    { "url" : "https://jezebel.com/elon-musks-trans-daughter-files-to-change-her-name-no-1849086311" },
+    { "url" : "https://www.cnn.com/2022/06/17/tech/spacex-letter-musk-firing/index.html" },
+    { "url" : "https://www.theverge.com/2022/6/17/23172913/spacex-complaint-letter-firing-elon-musk" },
+    { "url" : "https://www.motorbiscuit.com/elon-musk-wanted-tesla-co-founder-badly-took-lawsuit-make-legal-paper/" },
+    { "url" : "https://www.dailypress.com/business/ct-biz-tesla-braking-investigation-ap-20220603-lc5lygeia5hy3jbggzo32uewxq-story.html" },
+    { "url" : "https://www.theverge.com/2022/6/17/23172915/elon-musk-spacex-letter-fired-legal-protected-speech-nlrb" },
+    { "url" : "https://www.standard.co.uk/business/tesla-discrimination-elon-musk-racism-electric-cars-b1010093.html" },
+    { "url" : "https://www.cnn.com/2022/08/10/business/nightcap-elon-musk-spacex-cpi-dominos/index.html" },
+    { "url" : "https://www.seattletimes.com/business/elon-musks-antics-turn-owners-and-would-be-buyers-against-tesla/" },
+    { "url" : "https://news.yahoo.com/elon-musk-scolds-tesla-driver-153215907.html" },
+    { "url" : "https://www.cnbc.com/2022/08/05/california-dmv-says-tesla-fsd-autopilot-marketing-deceptive.html" },
+    { "url" : "https://www.usatoday.com/story/money/2022/09/15/tesla-elon-musk-sued-autopilot/10386771002/" },
+    { "url" : "https://arstechnica.com/tech-policy/2022/11/spacex-faces-labor-charges-after-firing-employees-who-criticized-elon-musk/" },
+    { "url" : "https://www.theverge.com/2022/11/20/23469616/tesla-recalls-over-300000-vehicles-taillight-issue-airbags" },
+    { "url" : "https://www.yahoo.com/news/tesla-issues-back-back-recalls-061456829.html" },
+    { "url" : "https://www.reuters.com/legal/exclusive-tesla-faces-us-criminal-probe-over-self-driving-claims-sources-2022-10-26/" },
+    { "url" : "https://www.popsci.com/technology/twitter-covid-misinformation/" },
+    { "url" : "https://www.theverge.com/23551060/elon-musk-twitter-takeover-layoffs-workplace-salute-emoji" },
+    { "url" : "https://www.msn.com/en-us/autos/news/emails-show-elon-musk-was-directly-involved-in-staged-self-driving-video/ar-AA16z1XM" },
+    { "url" : "https://www.nbcbayarea.com/news/local/east-bay/tesla-through-wall-san-ramon/3150489/" },
+    { "url" : "https://fortune.com/2023/02/09/steve-wozniak-says-dishonest-elon-musk-cheated-tesla-buyers/" },
+    { "url" : "https://www.platformer.news/p/elon-musk-fires-a-top-twitter-engineer" },
+    { "url" : "https://www.theguardian.com/technology/2023/feb/15/elon-musk-changes-twitter-algorithm-super-bowl-slump-report" },
+    { "url" : "https://www.forbes.com/sites/mattnovak/2023/02/20/yes-twitter-gives-a-warning-to-users-who-liked-a-tweet-criticizing-tesla-but-theres-more-to-the-story" },
+    { "url" : "https://www.nbcbayarea.com/news/local/east-bay/tesla-crash-automated-driving-system/3174530/" },
+    { "url" : "https://www.scmp.com/tech/big-tech/article/3231488/tesla-did-not-fix-autopilot-after-fatal-crash-engineers-say" },
+    { "url" : "https://www.theguardian.com/technology/2023/sep/08/elon-musk-committed-evil-starlink-order-ukraine" },
+// Videos        
+    { "url" : "https://www.youtube.com/watch?v=IAsWX8QKROo" }, // Elon Musk’s Neuralink Trials Go HORRIBLY Wrong - TYT
+    { "url" : "https://youtu.be/UsuYxFBHsiQ" }, // TOP TESLA FIRES COMPILATION VIDEO! Watch as these Tesla cars & batteries catch on fire and explode! - Random Earth
+    { "url" : "https://www.youtube.com/watch?v=7xzd_xlkPgw" }, //  Gravitas: 1500 animals killed in Neuralink trials - WION
+    { "url" : "https://www.msnbc.com/mehdi-on-msnbc/watch/what-elon-musk-s-china-ties-mean-for-tesla-twitter-and-how-it-could-endanger-the-rest-of-us-163939397764" },
+    { "url" : "https://www.youtube.com/watch?v=-Xv9AdwtfD8" }, // Fatal Tesla out-of-control crash - SCMP
+// Quora
+    { "url" : "https://www.quora.com/Whats-the-worst-thing-about-Elon-Musk" },
+// Tesla lawsuits
+    { "url" : "https://cdn.elonsucks.org/tesla/lynch-et-al-v-tesla-warn-act-class-action.pdf" },
+    { "url" : "https://cdn.elonsucks.org/tesla/isaacs-v-musk-tesla-securities-manipulation-class-action.pdf" },
+    { "url" : "https://cdn.elonsucks.org/tesla/williams-et-al-v-tesla-defective-suspension-class-action.pdf" },
+    { "url" : "https://cdn.elonsucks.org/tesla/california-v-tesla-racial-discrimination-complaint.pdf" },
+// Twitter lawsuits
+    { "url" : "https://cdn.elonsucks.org/twitter/cornet-et-al-v-twitter-warn-act-class-action.pdf" },
+    { "url" : "https://cdn.elonsucks.org/twitter/strifling-et-al-v-twitter-female-discrimination-class-action.pdf" }
+]
