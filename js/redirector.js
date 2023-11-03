a7af50273ece2588a61852ace381b60385822248
Adam Lui 刘展鹏 <adam@elonsucks.org>
2023-11-03 06:41:22 -0700

    Removed `var groups`, updated URLs var, replaced tabs w/ spaces, removed semicolons

--- js/redirector.js
+++ js/redirector.js
@@ -1,15 +1,12 @@
-// Count the number of URLs in list.
-var groups = redirectURLs.urls.length;
-
 // Randomize number and get the corresponding index for the URL
-var gotoUrl = redirectURLs.urls[Math.floor((Math.random() * groups))].url
+const gotoUrl = urls[Math.floor((Math.random() * urls.length))].url
 
 // Perform redirect
 if (window.location.search == "?debug") {
-	// Display URL instead of redirect
-	// if user appends ?debug
-	document.write("Redirect URL: " + gotoUrl);  
+    // Display URL instead of redirect
+    // if user appends ?debug
+    document.write("Redirect URL: " + gotoUrl)
 } else {
-	// Redirect to random URL
-	document.location = gotoUrl;
+    // Redirect to random URL
+    document.location = gotoUrl
 }
