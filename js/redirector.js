00938a966a59fc330fb66b3801f99537f1372fae
Adam Lui 刘展鹏 <adam@elonsucks.org>
2024-08-15 17:30:05 -0700

    Camel/upper-cased `gotoUrl`, removed semicolon

--- js/redirector.js
+++ js/redirector.js
@@ -1,12 +1,12 @@
 // Randomize number and get the corresponding index for the URL
-const gotoUrl = urls[Math.floor(Math.random() * urls.length)]; // eslint-disable-line
+const gotoURL = urls[Math.floor(Math.random() * urls.length)] // eslint-disable-line
 
 // Perform redirect
 if (window.location.search == '?debug') {
     // Display URL instead of redirect
     // if user appends ?debug
-    document.write('Redirect URL: ' + gotoUrl)
+    document.write('Redirect URL: ' + gotoURL)
 } else {
     // Redirect to random URL
-    document.location = gotoUrl
+    document.location = gotoURL
 }
