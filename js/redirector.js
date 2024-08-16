679c59d1f1ef1a7c0f152cac5bbbbc674083febb
Adam Lui 刘展鹏 <adam@elonsucks.org>
2024-08-15 17:32:21 -0700

    Removed comments/newlines for brevity

--- js/redirector.js
+++ js/redirector.js
@@ -1,7 +1,4 @@
-// Randomize number and get the corresponding index for the URL
 const randURL = urls[Math.floor(Math.random() * urls.length)] // eslint-disable-line
-
-// Perform redirect
 if (window.location.search == '?debug') // display random URL
     document.write('Redirect URL: ' + randURL)
 else // redir to random URL
