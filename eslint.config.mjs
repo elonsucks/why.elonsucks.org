fe0868b0c61e1c8db2e13703e4c15494ba8e8651
adamlui@protonmail.com <adamlui@protonmail.com>
2024-11-14 08:49:44 -0800

    Added eslint-plugin-regexp as dev dependency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,14 +2,16 @@ import js from '@eslint/js'
 import globals from 'globals'
 import json from '@eslint/json'
 import markdown from '@eslint/markdown'
+import * as regexp from 'eslint-plugin-regexp'
 import eslintPluginYml from 'eslint-plugin-yml'
 
 export default [
     {
         files: ['**/*.js', '**/*.mjs'],
         languageOptions: { ecmaVersion: 2022, sourceType: 'script', globals: { ...globals.browser }},
+        plugins: { regexp },
         rules: {
-            ...js.configs.recommended.rules,
+            ...js.configs.recommended.rules, ...regexp.configs['flat/recommended'].rules,
             'indent': 'off', 'no-unexpected-multiline': 'off', // allow whitespace anywhere
             'quotes': ['error', 'single', { 'allowTemplateLiterals': true }], // enforce single quotes except backticks to avoid escaping quotes
             'key-spacing': ['error', { 'beforeColon': false, 'afterColon': true }], // enforce spacing in object properties
