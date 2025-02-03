14e08228cbda822e816c467efeacc6903ea794de
adamlui@protonmail.com <adamlui@protonmail.com>
2025-02-03 11:48:19 -0800

    Added `?debug=all` mode

--- js/redirector.js
+++ js/redirector.js
@@ -16,10 +16,14 @@
 
     const urls = extractURLs(await (await fetch('assets/data/urls.json')).json()),
           randURL = urls[Math.floor(Math.random() * urls.length)]
-
-    if (location.search == '?debug') // display randURL
-        document.write(`Redirect URL: ${randURL}`)
-    else // redir to randURL
+    if (location.search.startsWith('?debug')) { // show randURL
+        let debugOutput = `<pre>Redirect URL (#${ urls.indexOf(randURL) +1 } of ${urls.length}): `
+                        + `<a href="${randURL}" style="color:blue">${randURL}</a></pre>`;
+        if (location.search.endsWith('=all')) // append `urls` array
+            debugOutput += `<pre>urls = ${JSON.stringify(urls, null, 2)
+                .replace(`"${randURL}"`, `<strong style="color:#48b720">"${randURL}"</strong>`)}</pre>`
+        document.write(debugOutput)
+    } else // redir to randURL
         document.location = randURL
 
 })()
