884b3ebf2d8f5337971c31171976943883e319c5
adamlui@protonmail.com <adamlui@protonmail.com>
2025-02-03 11:57:00 -0800

    Renamed `urlData` to `obj` for readability, improved comments

--- js/redirector.js
+++ js/redirector.js
@@ -1,15 +1,15 @@
 (async () => {
 
-    function extractURLs(urlData) {
+    function extractURLs(obj) {
         const urls = []
-        if (Array.isArray(urlData)) // contains URLs
-            urlData.forEach(entry => {
+        if (Array.isArray(obj)) // contains URLs
+            obj.forEach(entry => {
                 if (typeof entry == 'string') urls.push(entry) // push string URL
-                else urls.push(...extractURLs(entry)) // recurse to push entry.url
+                else urls.push(...extractURLs(entry)) // recurse once to push entry.url
             })
         else { // contains `url` key or needs further recursion
-            if (urlData.url) urls.push(urlData.url) // push found `url` key's val
-            else Object.values(urlData).forEach(value => urls.push(...extractURLs(value))) // recurse to reach arrays
+            if (obj.url) urls.push(obj.url) // push found obj.url's val
+            else Object.values(obj).forEach(value => urls.push(...extractURLs(value))) // recurse to reach arrays
         }
         return urls
     }
