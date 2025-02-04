ff0edbb87a076d0f8f62fdc16161503e732ce9f0
adamlui@protonmail.com <adamlui@protonmail.com>
2025-02-04 12:06:35 -0800

    Removed no-longer used `async` from `exec.stdout.on()` callback

--- utils/preview.js
+++ utils/preview.js
@@ -3,7 +3,7 @@
 (async () => {
     const open = (await import('open')).default
 
-    require('child_process').exec('http-server').stdout.on('data', async data => {
+    require('child_process').exec('http-server').stdout.on('data', data => {
         console.log(data)
         if (data.includes('Starting up http-server')) // preview once (on 1st stdout msg)
             open(`http://localhost:8080${process.argv.includes('--debug') ? '?debug=all' : ''}`)
