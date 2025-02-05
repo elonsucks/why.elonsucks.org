81a2d7f0dd04ed4364a81fa4359d251061e3e095
adamlui@protonmail.com <adamlui@protonmail.com>
2025-02-05 15:38:34 -0800

    Replaced `data.includes()` w/ `.startsWith()` for stricter matching

--- utils/preview.js
+++ utils/preview.js
@@ -5,8 +5,8 @@
 
     require('child_process').exec('http-server').stdout.on('data', data => {
         console.log(data)
-        if (data.includes('Starting up http-server')) // preview once (on 1st stdout msg)
-            open(`http://localhost:8080${process.argv.includes('--debug') ? '?debug=all' : ''}`)
+        if (data.startsWith('Starting up http-server')) // preview once (on 1st stdout msg)
+            open(`http://localhost:8080${ process.argv.includes('--debug') ? '?debug=all' : '' }`)
     })
 
 })()
