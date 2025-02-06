7c0a715026624ab0c38443e4ff7857c5b6130157
adamlui@protonmail.com <adamlui@protonmail.com>
2025-02-05 18:22:51 -0800

    Replaced loopback/network address logs w/ single `localhost` one

--- utils/preview.js
+++ utils/preview.js
@@ -8,14 +8,15 @@
 
     // Init UI COLORS
     const bw = '\x1b[97m', // bright white
+          by = '\x1b[93m', // bright yellow
           nc = '\x1b[0m'   // no color
 
     // PREVIEW page
-    const availPort = await getPort({ port: portNumbers(3000, 3999) }),
-          stopHint = 'Hit CTRL-C to stop the server'
+    const availPort = await getPort({ port: portNumbers(3000, 3999) })
     require('child_process').exec(`http-server -p ${availPort}`).stdout.on('data', data => {
-        if (data.includes(stopHint)) { // server ready msg, enrich then preview
-            data = data.replace(stopHint, `\nPress ${bw}CTRL+C${nc} in terminal to stop server`)
+        if (data.includes('Hit CTRL-C to stop the server')) { // server ready msg, enrich then preview
+            data = data.replace(/Available on:[\s\S]*/, `Listening at ${by}http://localhost:${availPort}${nc}\n\n`)
+                 + `Press ${bw}CTRL+C${nc} in terminal to stop server\n`
                  + `Press ${bw}CTRL+SHIFT+R${nc} in browser to clear cache (if assets/data/urls.json updated)\n`
             open(`http://localhost:${availPort}${ process.argv.includes('--debug') ? '?debug=all' : '' }`)
         }
