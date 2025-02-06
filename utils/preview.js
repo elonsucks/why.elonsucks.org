b30eb35156f26f7a1405fde19c43c1645e61b52c
adamlui@protonmail.com <adamlui@protonmail.com>
2025-02-05 17:02:16 -0800

    Randomized port selection to available one within 3000-3999

--- utils/preview.js
+++ utils/preview.js
@@ -1,19 +1,23 @@
 #!/usr/bin/env node
 
 (async () => {
-    const open = (await import('open')).default
+
+    // Import LIBS
+    const open = (await import('open')).default,
+          { default: getPort, portNumbers } = await import('get-port')
 
     // Init UI COLORS
     const bw = '\x1b[97m', // bright white
           nc = '\x1b[0m'   // no color
 
     // PREVIEW page
-    const stopHint = 'Hit CTRL-C to stop the server'
-    require('child_process').exec('http-server').stdout.on('data', data => {
+    const availPort = await getPort({ port: portNumbers(3000, 3999) }),
+          stopHint = 'Hit CTRL-C to stop the server'
+    require('child_process').exec(`http-server -p ${availPort}`).stdout.on('data', data => {
         if (data.includes(stopHint)) { // stdout msg when server ready
             data = data.replace('Hit CTRL-C to stop the server', `\nPress ${bw}CTRL+C${nc} in terminal to stop server`)
             data += `Press ${bw}CTRL+SHIFT+R${nc} in browser to clear cache (if assets/data/urls.json updated)`
-            open(`http://localhost:8080${ process.argv.includes('--debug') ? '?debug=all' : '' }`)
+            open(`http://localhost:${availPort}${ process.argv.includes('--debug') ? '?debug=all' : '' }`)
         }
         console.log(data)
     })
