6157c3ac6e45d9a8c398077ce340fce003578918
adamlui@protonmail.com <adamlui@protonmail.com>
2025-02-05 16:43:09 -0800

    Colored keyboard cmds bright white, enclosed assets/data/urls.json note in parentheses

--- utils/preview.js
+++ utils/preview.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
 
 (async () => {
-    const open = (await import('open')).default,
-          stopHint = 'Hit CTRL-C to stop the server'
+    const open = (await import('open')).default
 
+    // Init UI COLORS
+    const bw = '\x1b[97m', // bright white
+          nc = '\x1b[0m'   // no color
+
+    // PREVIEW page
+    const stopHint = 'Hit CTRL-C to stop the server'
     require('child_process').exec('http-server').stdout.on('data', data => {
         if (data.includes(stopHint)) { // stdout msg when server ready
-            data = data.replace('Hit CTRL-C to stop the server', '\nPress CTRL+C in terminal to stop server')
-            data += 'Press CTRL+SHIFT+R in browser to clear cache if assets/data/urls.json updated'
+            data = data.replace('Hit CTRL-C to stop the server', `\nPress ${bw}CTRL+C${nc} in terminal to stop server`)
+            data += `Press ${bw}CTRL+SHIFT+R${nc} in browser to clear cache (if assets/data/urls.json updated)`
             open(`http://localhost:8080${ process.argv.includes('--debug') ? '?debug=all' : '' }`)
         }
         console.log(data)
