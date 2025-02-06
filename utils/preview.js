86947b22e28b37898c2d8ec3de4081005aa57012
adamlui@protonmail.com <adamlui@protonmail.com>
2025-02-05 22:45:34 -0800

    Replaced spawn w/ cross-spawn to eliminate cross-platform `cmds.spawn`

--- utils/preview.js
+++ utils/preview.js
@@ -3,21 +3,14 @@
 (async () => {
 
     // Import LIBS
-    const os = require('os'), // for init cmds
-          { default: getPort, portNumbers } = await import('get-port'), // for availPort
+    const { default: getPort, portNumbers } = await import('get-port'), // for availPort
+          spawn = require('cross-spawn'), // to preserve cmd prompt
           open = (await import('open')).default // for preview in exec().stdout.on()
 
     // Init UI COLORS
     const bw = '\x1b[97m', // bright white
           by = '\x1b[93m', // bright yellow
           nc = '\x1b[0m'   // no color
-
-    // Init CMDS
-    const cmds = { node: `node ${__filename} --recursing` }
-    cmds.spawn = os.platform() == 'darwin' ? `osascript -e 'tell app "Terminal" to do script "${cmds.node}"'`
-               : os.platform() == 'win32'  ? `start cmd /k "${cmds.node}"`
-               : ( /* linux */ `x-terminal-emulator -e bash -c "${cmds.node} ; exec bash"`
-                              + ` || gnome-terminal -- bash -c "${cmds.node} ; exec bash"` )
 
     // Init STDOUT vars
     const availPort = await getPort({ port: portNumbers(3000, 3999) })
@@ -26,10 +19,10 @@
         + `Press ${bw}CTRL+C${nc} in terminal to stop server\n`
         + `Press ${bw}CTRL+SHIFT+R${nc} in browser to clear cache (if assets/data/urls.json updated)\n`
 
-    // SPAWN new terminal from OG one (to preserve cmd prompt)
-    if (!process.argv.includes('--recursing'))
-        return require('child_process').spawn(cmds.spawn, { shell: true, detached: true, stdio: 'ignore' })
+    if (!process.argv.includes('--recursing')) // spawn new terminal from OG one
+        return spawn(`node ${__filename} --recursing`, { shell: true, detached: true, stdio: 'ignore' })
             .on('error', err => console.error(`Failed to open new terminal: ${err.message}`))
+            .unref() // detach process to allow immediate return to cmd prompt
 
     // PREVIEW page
     require('child_process').exec(`http-server -p ${availPort}`).stdout.on('data', data => {
