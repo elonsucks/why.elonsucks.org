70f5cb03a3a2dcf99eba38769ceedccf5687c219
adamlui@protonmail.com <adamlui@protonmail.com>
2025-02-05 18:49:31 -0800

    Consolidated server readiness check w/ access info replacement

--- utils/preview.js
+++ utils/preview.js
@@ -12,10 +12,11 @@
           nc = '\x1b[0m'   // no color
 
     // PREVIEW page
-    const availPort = await getPort({ port: portNumbers(3000, 3999) })
+    const availPort = await getPort({ port: portNumbers(3000, 3999) }),
+          reAccessInfo = /Available on:[\s\S]+/
     require('child_process').exec(`http-server -p ${availPort}`).stdout.on('data', data => {
-        if (data.includes('Hit CTRL-C to stop the server')) { // server ready msg, enrich then preview
-            data = data.replace(/Available on:[\s\S]*/, `Listening at ${by}http://localhost:${availPort}${nc}\n\n`)
+        if (reAccessInfo.test(data)) { // server ready msg, enrich then preview
+            data = data.replace(reAccessInfo, `Listening at ${by}http://localhost:${availPort}${nc}\n\n`)
                  + `Press ${bw}CTRL+C${nc} in terminal to stop server\n`
                  + `Press ${bw}CTRL+SHIFT+R${nc} in browser to clear cache (if assets/data/urls.json updated)\n`
             open(`http://localhost:${availPort}${ process.argv.includes('--debug') ? '?debug=all' : '' }`)
